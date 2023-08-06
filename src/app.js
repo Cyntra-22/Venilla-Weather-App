40d0107001e38a15beebbbd7c216fd2bfdf6f62c
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-08-06 14:28:23 +0700

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function formatDate(timestemp) {
   //calculate Date
   let date = new Date(timestemp);
   let hours = date.getHours();
-  if (hours < 10){
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
@@ -19,15 +19,15 @@ function formatDate(timestemp) {
     "Friday",
     "Saturday",
   ];
-  let index=date.getDay();
+  let index = date.getDay();
   let day = days[index];
   return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
- 
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  celciusTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
@@ -45,30 +45,50 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.time * 1000);
 
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute("src",response.data.condition.icon_url);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
 
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-function search(city){
-
+function search(city) {
   let apiKey = "d01eb3ad3ceo6b4c8t9cf27d410a60cd";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
-
 }
 
-
-
-
-function handleSubmit(event){
+function handleSubmit(event) {
   event.preventDefault();
   let cityinputElement = document.querySelector("#city-input");
   search(cityinputElement.value);
 }
 
-
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  //remove the active class
+  celciusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelciusTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let celciusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", showCelciusTemperature);
