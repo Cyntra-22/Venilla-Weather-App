a151cb6db4cbaa29e9d93f16ce7e38ab7772d531
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-08-06 11:27:41 +0700

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -50,9 +50,25 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "d01eb3ad3ceo6b4c8t9cf27d410a60cd";
-let city = "Japan";
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function search(city){
 
-axios.get(apiUrl).then(displayTemperature);
+  let apiKey = "d01eb3ad3ceo6b4c8t9cf27d410a60cd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+
+}
+
+
+
+
+function handleSubmit(event){
+  event.preventDefault();
+  let cityinputElement = document.querySelector("#city-input");
+  search(cityinputElement.value);
+}
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
